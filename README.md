1562d1eda6836f3f5fe0ff79ebc9916b121d9549
Martin van Leussen <martin@vanleussen.com>
2019-10-08 19:26:54 +0200

    First version of working app.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # brotherprint-label-server
 Receives http get requests and initiate to print label on brother label printer with template. 
+
+Start
+```
+docker-compose up -d --build
+```
+
+Stop
+```
+docker-compose down
+```
---
+++ app/source/app.py
@@ -0,0 +1,33 @@
+# app.py
+from flask import Flask, request
+from brotherprint import BrotherPrint
+import socket
+
+tempNumber = 1
+printerIP = '192.168.2.20'
+app = Flask(__name__)
+
+@app.route('/labelserver')
+def labelserver():
+  box = request.args.get('box')
+  barcode = request.args.get('barcode')
+  name = request.args.get('name')
+
+  f_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  f_socket.connect((printerIP, 9100))
+  printjob = BrotherPrint(f_socket)
+  printjob.template_mode()
+  printjob.template_init()
+  printjob.choose_template(tempNumber)
+  printjob.select_and_insert('box', box)
+  printjob.select_and_insert('barcode', barcode)
+  printjob.select_and_insert('name', name)
+  printjob.template_print()
+
+  return '''<h1>Printing label</h1>
+            <p>Box: {}<br>
+            Barcode: {}<br>
+            Name: {}</p>'''.format(box,barcode,name)
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
